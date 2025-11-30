15ca98de5ef87be17f44618be960b260382805dc
Samuel Mbano <121202097+samzypaul@users.noreply.github.com>
2025-11-30 15:29:55 +0300

    first commit

---
+++ User/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import loader
+
+
+def home_page(request):
+    template = loader.get_template("home.html")
+    context = {}
+    return HttpResponse(template.render({},request))# render(request,"templates/home.html")#
